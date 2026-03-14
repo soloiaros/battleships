b4c2daf6aa66b4f6b7a4a24ac50261c05c44286c
soloiaros <soloiaros@gmail.com>
2026-03-14 17:29:48 +0300

    feat: implement screen manager class
    This class is the main driver of the game that processes all user
    interactions with the board elements and leads the player through the
    game step by step

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./main.scss";
 import setUpEventListeners from "./interface";
+import ScreenManager from "./screen-manager";
 
 setUpEventListeners();
+const manager = new ScreenManager();
---
+++ src/screen-manager.js
@@ -0,0 +1,102 @@
+import { HumanPlayer, ComputerPlayer } from "./base-classes";
+
+export default class ScreenManager {
+  constructor() {
+    this.humanPlayer = new HumanPlayer();
+    this.compPlayer = new ComputerPlayer();
+    this.currDefender = this.compPlayer;
+
+    const opponentBoard = document.querySelector('.board-container:nth-child(2) table');
+    for (let i = 1; i <= 10; i++) {
+      for (let j = 1; j <= 10; j++) {
+        const tableCell = opponentBoard.querySelector(`table tr:nth-child(${i}) td:nth-child(${j}) div`);
+        tableCell.addEventListener('click', () => {
+          this.processAttack([i - 1, j - 1]);
+        })
+      }
+    }
+
+    const startBtn = document.getElementById('start-btn');
+    startBtn.addEventListener('click', () => { this.startGame(); });
+  }
+
+  processAttack(YXPair) {
+    if (!this.currDefender.board.canShoot(YXPair)) return;
+
+    this.currDefender.board.shoot(YXPair);
+    this.updateBoard();
+
+    if (!this.currDefender.board.shipsAlive) this.stopGame();
+    else {
+      this.currDefender = this.currDefender === this.compPlayer ? this.humanPlayer : this.compPlayer;
+      this.takeTurn();
+    }
+    
+  }
+
+  startGame() {
+    this.setUpBoards();
+    this.takeTurn();
+    this.updateBoard();
+  }
+
+  setUpBoards() {
+    // For temporary testing only
+    this.humanPlayer.board.placeShip([1, 1], [1, 2], [1, 3], [1, 4])
+    this.humanPlayer.board.placeShip([5, 5], [6, 5])
+    this.compPlayer.board.placeShip([2, 2], [3, 2], [4, 2], [5, 2])
+    this.compPlayer.board.placeShip([7, 7], [7, 8])
+  }
+
+  stopGame() {
+    const startBtn = document.getElementById('start-btn');
+    const turnDisplay = document.getElementById('turn-display');
+    
+    startBtn.style.display = 'block';
+    turnDisplay.style.display = 'none';
+
+    document.querySelector(`.board-container:nth-child(1) table`).classList = 'inactive';
+    document.querySelector(`.board-container:nth-child(2) table`).classList = 'inactive';
+  }
+
+  takeTurn() {
+    const turnDisplay = document.getElementById('turn-display');
+    turnDisplay.textContent = `It's ${this.currDefender === this.compPlayer ? 'your' : "opponent's"} turn.`;
+    const [activeBoardIndex, inactiveBoardIndex] = this.currDefender === this.compPlayer ? [2, 1] : [1, 2];
+    const activeBoard = document.querySelector(`.board-container:nth-child(${activeBoardIndex}) table`);
+    const inactiveBoard = document.querySelector(`.board-container:nth-child(${inactiveBoardIndex}) table`);
+
+    activeBoard.classList = 'active';
+    inactiveBoard.classList = 'inactive';
+
+    if (this.currDefender === this.humanPlayer) {
+      this.initiateCompMove();
+    }
+  }
+
+  initiateCompMove() {
+    const availableCells = this.humanPlayer.board.emptyCells;
+    const targetCellIndex = availableCells.at(Math.floor(Math.random() * availableCells.length));
+    this.processAttack(targetCellIndex);
+  }
+
+  updateBoard() {
+    const shotMap = this.currDefender.board.shotMap;
+    const shipMap = this.currDefender.board.shipMap;
+    const board = document.querySelector(`.board-container:nth-child(${this.currDefender === this.compPlayer ? 2 : 1}) table`);
+
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        const tableCell = board.querySelector(`table tr:nth-child(${i + 1}) td:nth-child(${j + 1}) div`);
+        if (shotMap[i][j]) {
+          if (!tableCell.classList.contains('hit')) tableCell.classList.add('hit');
+        }
+        if (shipMap[i][j]) {
+          if (!tableCell.classList.contains('ship')) tableCell.classList.add('ship');
+        }
+      }
+    }
+  }
+
+
+  }
