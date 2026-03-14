4d90f41c23059c7422377a5a76d1fc3f6bcb0297
soloiaros <soloiaros@gmail.com>
2026-03-14 11:44:00 +0300

    feat: define board class & basic functionality
    Some of the basic methods and properties were implemented to satisfy the
    initial test suite

--- src/game.js
+++ src/game.js
@@ -12,3 +12,39 @@ export class Ship {
     this.hits += 1;
   }
 }
+
+export class Gameboard {
+  constructor() {
+    this.shotMap = new Array();
+    for (let i = 0; i < 10; i++) {
+      this.shotMap.push(new Array());
+      for (let j = 0; j < 10; j++)  {
+        this.shotMap.at(i).push(0);
+      }
+    }
+
+    this.shipMap = new Array();
+    for (let i = 0; i < 10; i++) {
+      this.shipMap.push(new Array());
+      for (let j = 0; j < 10; j++)  {
+        this.shipMap.at(i).push(null);
+      }
+    }
+  }
+
+  placeShip(...coords) {
+    const ship = new Ship(coords.length);
+    for (let YXPair of coords) {
+      this.shipMap[YXPair.at(0)][YXPair.at(1)] = ship;
+    }
+  }
+
+  shoot(YXPair) {
+    this.shotMap[YXPair.at(0)][YXPair.at(1)] = 1;
+    if (this.shipMap[YXPair.at(0)][YXPair.at(1)]) this.shipMap[YXPair.at(0)][YXPair.at(1)].hit();
+  }
+
+  canShoot(YXPair) {
+    return YXPair.every((coord) => coord >= 0 && coord < 10) && !this.shotMap[YXPair.at(0)][YXPair.at(1)];
+  }
+}
